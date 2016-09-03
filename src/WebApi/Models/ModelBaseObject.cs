9e94d3ce8df8838ca0c68784d6d1a7c55255b7d7
andrekirst <github@andrekirst.de>
2016-09-03 16:12:25 +0200

    #6 - BasisObjekt angelegt

---
+++ src/WebApi/Models/ModelBaseObject.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright André Kirst
+ * 2016 - 2016
+ * School
+*/
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class ModelBaseObject
+    {
+        /// <summary>
+        /// Ist die Id des Mongo-DB-Objektes
+        /// </summary>
+        public ObjectId Id
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Das Datum, bei dem das Objekt entstanden ist
+        /// </summary>
+        public DateTime CreatedAt
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Das Datum, bei dem das Objekt zuletzt geändert wurde
+        /// </summary>
+        public DateTime? ModifiedAt
+        {
+            get; set;
+        }
+    }
+}
