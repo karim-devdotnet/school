5e562350ab26efd506e8ca77b4f56da40999a97c
andrekirst <github@andrekirst.de>
2016-09-17 15:13:04 +0200

    #6 - Skript für automatisches syncen

---
+++ git_sync.ps1
@@ -0,0 +1,6 @@
+Write-Output "git fetch origin --tags"
+git fetch origin --tags;
+Write-Output "git push origin --all"
+git push origin --all;
+Write-Output "git push origin --tags"
+git push origin --tags;
