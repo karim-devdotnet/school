da8fe3b0881bf14d1207a33bcec93eabd2532a9f
andrekirst <github@andrekirst.de>
2016-09-27 21:39:00 +0200

    #5 - restore src/WebClient

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ if [ -d $artifactsFolder ]; then
 fi
 
 dotnet restore ./src/WebApi
-#dotnet restore ./src/WebClient
+dotnet restore ./src/WebClient
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
