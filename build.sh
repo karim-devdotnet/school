eff397440a567c703e99b1f12a95b7a972ed460d
andrekirst <github@andrekirst.de>
2016-09-27 15:51:20 +0200

    #5 - Anpassung build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
-dotnet restore
+dotnet restore ./src/WebApi
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
