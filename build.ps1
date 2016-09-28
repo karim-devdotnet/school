8b814da14ffc3610ca1856a1268dc44afcf3c4c6
andrekirst <github@andrekirst.de>
2016-09-28 21:15:20 +0200

    #5 - Anpassung WebClient-Project, build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,16 @@
 # Build-Skript für Windows mit PowerShell
+
+$buildOutputDirectoryName = "buildOutput";
+
+$buildOutputDirectoryPath = $PSScriptRoot + "\" + $buildOutputDirectoryName + "\";
+
+Write-Host "Build-Directory-Output-Path: "$buildOutputDirectoryPath;
+
+$webApiNameString = "WebApi";
+$webClientNameString = "WebClient";
+
+dotnet --verbose restore $PSScriptRoot/src/$webApiNameString;
+dotnet --verbose build $PSScriptRoot/src/$webApiNameString;
+
+dotnet --verbose restore $PSScriptRoot/src/$webClientNameString;
+dotnet --verbose build $PSScriptRoot/src/$webClientNameString;
