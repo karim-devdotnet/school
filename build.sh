648ea637de9fb32cefb4961456ab2579f22db814
andrekirst <github@andrekirst.de>
2016-09-28 16:01:45 +0200

    #5 - Anpassungen und Entfernungen

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 #exit if any command fails
 set -e
 
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then
+  rm -R $artifactsFolder
+fi
+
 sh ./src/WebApi/build.sh
 
 sh ./src/WebClient/build.sh
--- src/WebClient/build.sh
+++ src/WebClient/build.sh
@@ -3,12 +3,6 @@
 #exit if any command fails
 set -e
 
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then
-  rm -R $artifactsFolder
-fi
-
 #dotnet restore ./src/WebApi
 dotnet restore ./src/WebClient
 
