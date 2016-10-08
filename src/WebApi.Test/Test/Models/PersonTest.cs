eaea810a38ef3a11358be06f6358f2487375531f
andrekirst <github@andrekirst.de>
2016-10-08 23:25:02 +0200

    #19 - Testklassen erstellt - ! müssen noch befüllt werden !

---
+++ src/WebApi.Test/Test/Models/PersonTest.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Copyright André Kirst
+ * 2016 - 2016
+ * School
+*/
+using Xunit;
+
+namespace WebApi.Test.Test.Models
+{
+    /// <summary>
+    /// Test-Klasse für das Objekt Person
+    /// </summary>
+    public class PersonTest : IClassFixture<TestFixture>
+    {
+    }
+}
---
+++ src/WebApi.Test/Test/Models/VerwaltungsangestellterTest.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Copyright André Kirst
+ * 2016 - 2016
+ * School
+*/
+using Xunit;
+
+namespace WebApi.Test.Test.Models
+{
+    /// <summary>
+    /// Test-Klasse für das Objekt Verwaltungsangestellter
+    /// </summary>
+    public class VerwaltungsangestellterTest : IClassFixture<TestFixture>
+    {
+    }
+}
