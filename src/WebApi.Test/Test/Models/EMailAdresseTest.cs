6251ad1b4f85bf93113940d3e6412aff1b643caa
andrekirst <github@andrekirst.de>
2016-10-09 12:52:11 +0200

    #19 - Änderung der Testfälle für E-Mail-Adressen

--- src/WebApi.Test/Test/Models/EMailAdresseTest.cs
+++ src/WebApi.Test/Test/Models/EMailAdresseTest.cs
@@ -3,33 +3,67 @@
  * 2016 - 2016
  * School
 */
+using System.Collections.Generic;
 using WebApi.Models;
 using Xunit;
+using Xunit.Extensions;
 
 namespace WebApi.Test.Models
 {
     public class EMailAdresseTest : IClassFixture<TestFixture>
     {
         /// <summary>
-        /// Testen der E-Mail-Adresse auf ihre Gültigkeit
+        /// Eine Theorie für gültige E-Mail-Adresse
         /// </summary>
-        [Fact]
-        public void ValidEMailAdresse()
+        /// <param name="emailadresse"></param>
+        [Theory, MemberData("GueltigeEMailAdressdaten")]
+        [Trait("Komponente", "EMailAdresse")]
+        [Trait("UnitTestArt", "Positiv-Test")]
+        public void GueltigeEMailAdressen(string emailadresse)
         {
             EMailAdresse adresse = new EMailAdresse();
-            adresse.Adresse = "test@test.de";
+            adresse.Adresse = emailadresse;
             Assert.True(adresse.IsValid);
         }
 
         /// <summary>
-        /// Testen der E-Mail-Adresse auf ihre Ungültigkeit
+        /// Gültige E-Mail-Adressen
         /// </summary>
-        [Fact]
-        public void InvalidEMailAdresse()
+        /// <returns></returns>
+        public static IEnumerable<object[]> GueltigeEMailAdressdaten()
+        {
+            yield return new object[] { "test@test.de" };
+            yield return new object[] { "test-test@test.de" };
+            yield return new object[] { "test@test-test.de" };
+            yield return new object[] { "test-test@test-test.de" };
+        }
+
+        /// <summary>
+        /// Eine Theorie für ungültige EMail-Adresse
+        /// </summary>
+        /// <param name="emailadresse"></param>
+        [Theory, MemberData("UngueltigeEMailAdressdaten")]
+        [Trait("Komponente", "EMailAdresse")]
+        [Trait("UnitTestArt", "Negativ-Test")]
+        public void UngueltigeEMailAdressen(string emailadresse)
         {
             EMailAdresse adresse = new EMailAdresse();
-            adresse.Adresse = "test-test.de";
+            adresse.Adresse = emailadresse;
             Assert.False(adresse.IsValid);
+        }
+
+        /// <summary>
+        /// Ungültige E-Mail-Adressen
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<object[]> UngueltigeEMailAdressdaten()
+        {
+            yield return new object[] { "test@testde" };
+            yield return new object[] { "test-test.de" };
+            yield return new object[] { "test-testde" };
+            yield return new object[] { "test@@testde" };
+            yield return new object[] { "test@@test.de" };
+            yield return new object[] { "testattest.de" };
         }
     }
 }
