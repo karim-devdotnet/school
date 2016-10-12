696bf885dae549877419301fa7082e67f40f18fe
andrekirst <github@andrekirst.de>
2016-10-12 21:11:43 +0200

    #19 - Neue Tests für Adressen

--- src/WebApi.Test/Test/Models/AdresseTest.cs
+++ src/WebApi.Test/Test/Models/AdresseTest.cs
@@ -30,12 +30,16 @@ namespace WebApi.Test.Test.Models
                 SchoolContext.Instance().SaveItem<Adresse>(adresse);
                 Assert.True(true);
             }
-            catch(Exception)
+            catch(Exception ex)
             {
                 Assert.False(false);
             }
         }
 
+        /// <summary>
+        /// Test für gültige Adressen
+        /// </summary>
+        /// <param name="adresse"></param>
         [Theory, MemberData(nameof(GueltigeAdresseDaten))]
         [Trait("Komponente", "Adresse")]
         [Trait("UnitTestArt", "Positiv-Test")]
@@ -43,7 +47,7 @@ namespace WebApi.Test.Test.Models
         {
             Assert.True(adresse.IstGueltig);
         }
-        
+
         /// <summary>
         /// Gültige E-Mail-Adressen
         /// </summary>
@@ -59,6 +63,102 @@ namespace WebApi.Test.Test.Models
                     Bundesland = new Bundesland() { Ident = "EUROPA_DEUTSCHLAND_SACHSEN", Name = "Sachsen", Land = new WebApi.Models.Land() { Ident = "EUROPA_DEUTSCHLAND", Name = "Deutschland" } },
                     Ort = "Musterhausen",
                     Postleitzahl = "12345"
+                }
+            };
+        }
+
+
+        /// <summary>
+        /// Test für ungültige Adressen
+        /// </summary>
+        /// <param name="adresse"></param>
+        [Theory, MemberData(nameof(UngueltigeAdresseDaten))]
+        [Trait("Komponente", "Adresse")]
+        [Trait("UnitTestArt", "Negativ-Test")]
+        public void UngueltigeAdresse(Adresse adresse)
+        {
+            Assert.False(adresse.IstGueltig);
+        }
+
+        /// <summary>
+        /// Ungültige E-Mail-Adressen
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<object[]> UngueltigeAdresseDaten()
+        {
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Strasse = "Musterstraße",
+                    Bundesland = new Bundesland() { Ident = "EUROPA_DEUTSCHLAND_SACHSEN", Name = "Sachsen", Land = new Land() { Ident = "EUROPA_DEUTSCHLAND", Name = "Deutschland" } },
+                    Ort = "Musterhausen",
+                    Postleitzahl = "12345"
+                }
+            };
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Bundesland = new Bundesland() { Ident = "EUROPA_DEUTSCHLAND_SACHSEN", Name = "Sachsen", Land = new Land() { Ident = "EUROPA_DEUTSCHLAND", Name = "Deutschland" } },
+                    Ort = "Musterhausen",
+                    Postleitzahl = "12345"
+                }
+            };
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Bundesland = new Bundesland() { Ident = "EUROPA_DEUTSCHLAND_SACHSEN", Name = "Sachsen", Land = new Land() { Ident = "EUROPA_DEUTSCHLAND", Name = "Deutschland" } },
+                    Postleitzahl = "12345"
+                }
+            };
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Bundesland = new Bundesland() { Ident = "EUROPA_DEUTSCHLAND_SACHSEN", Name = "Sachsen", Land = new Land() { Ident = "EUROPA_DEUTSCHLAND", Name = "Deutschland" } }
+                }
+            };
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Strasse = "Musterstraße",
+                    Hausnummer = "42",
+                    Ort = "Musterhausen",
+                    Postleitzahl = "12345"
+                }
+            };
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Strasse = "Musterstraße",
+                    Hausnummer = "42",
+                    Ort = "Musterhausen",
+                    Postleitzahl = "1234"
+                }
+            };
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Strasse = "",
+                    Hausnummer = "",
+                    Ort = "",
+                    Postleitzahl = ""
+                }
+            };
+            yield return new object[]
+            {
+                new Adresse()
+                {
+                    Strasse = " ",
+                    Hausnummer = " ",
+                    Bundesland = new Bundesland() { Ident = "EUROPA_DEUTSCHLAND_SACHSEN", Name = "Sachsen", Land = new WebApi.Models.Land() { Ident = "EUROPA_DEUTSCHLAND", Name = "Deutschland" } },
+                    Ort = " ",
+                    Postleitzahl = " "
                 }
             };
         }
--- src/WebApi.Test/Test/Models/LandTest.cs
+++ src/WebApi.Test/Test/Models/LandTest.cs
@@ -3,6 +3,11 @@
  * 2016 - 2016
  * School
 */
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using WebApi.Base;
+using WebApi.Models;
 using Xunit;
 
 namespace WebApi.Test.Test.Models
@@ -12,5 +17,56 @@ namespace WebApi.Test.Test.Models
     /// </summary>
     public class LandTest : IClassFixture<TestFixture>
     {
+        [Theory]
+        [MemberData(nameof(GueltigeLandDaten))]
+        [Trait("Komponente", "Land")]
+        [Trait("UnitTestArt", "Positiv-Test")]
+        public void LandSpeichern(Land land)
+        {
+            try
+            {
+                BsonDocument allFilter = new BsonDocument();
+                SchoolContext.Instance().GetCollection<Land>().DeleteMany(allFilter);
+
+                SchoolContext.Instance().SaveItem<Land>(land);
+                Assert.True(true);
+            }
+            catch(Exception ex)
+            {
+                Assert.False(false);
+            }
+        }
+
+        /// <summary>
+        /// Gültige Länder
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<object[]> GueltigeLandDaten()
+        {
+            yield return new object[]
+            {
+                new Land()
+                {
+                    Ident = "EUROPA_DEUTSCHLAND",
+                    Name = "Deutschland"
+                }
+            };
+            yield return new object[]
+            {
+                new Land()
+                {
+                    Ident = "EUROPA_ENGLAND",
+                    Name = "England"
+                }
+            };
+            yield return new object[]
+            {
+                new Land()
+                {
+                    Ident = "EUROPA_OESTERREICH",
+                    Name = "Österreich"
+                }
+            };
+        }
     }
 }
