61f11db33a955d207d4154448ca1f41a24ff0d6c
andrekirst <github@andrekirst.de>
2016-10-16 11:39:32 +0200

    #39 - Anpassung SonarQube-Skript

--- analyseSonarQube.sh
+++ analyseSonarQube.sh
@@ -2,16 +2,6 @@
 # Beenden bei Fehler
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    echo "SonarQube-Anylyse..."
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -B -e -V -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "Pull-Request - SonarQube-Analyse..."
-    mvn clean package sonar:sonar -B -e -V \
-		-Dsonar.host.url=$SONAR_HOST_URL \
-		-Dsonar.login=$SONAR_TOKEN \
-		-Dsonar.analysis.mode=preview \
-		-Dsonar.github.oauth=$GITHUB_TOKEN \
-		-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
-fi
+echo "Start SonarQube-Analyse..."
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -B -e -V -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+echo "Ende SonarQube-Analyse..."
