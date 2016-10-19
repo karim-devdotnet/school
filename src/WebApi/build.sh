0ef2c25ad1c6dc6427db26978cf56a7cb44b4edd
andrekirst <github@andrekirst.de>
2016-10-19 16:38:08 +0200

    #19 - -fnet460 -> -f net460

--- src/WebApi/build.sh
+++ src/WebApi/build.sh
@@ -8,7 +8,7 @@ dotnet restore ./src/WebApi
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet restore ./src/WebApi.Test -fnet460
+dotnet restore ./src/WebApi.Test -f net460
 
 dotnet build ./src/WebApi.Test -c Release -f net460
 
