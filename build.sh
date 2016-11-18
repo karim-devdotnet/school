f6b6584eaf0bb4e95b68a37e7922ee40b65f599b
andrekirst <github@andrekirst.de>
2016-11-18 23:12:35 +0100

    #53 - Änderungen auf net core

--- build.sh
+++ build.sh
@@ -12,3 +12,7 @@ fi
 sh ./src/WebApi/build.sh
 
 sh ./src/WebClient/build.sh
+
+chmod ugo=rx ./dockerfiles/webapi/linux/docker_build.sh 
+
+sh ./dockerfiles/webapi/linux/docker_build.sh
--- dockerfiles/webapi/linux/docker_build.sh
+++ dockerfiles/webapi/linux/docker_build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t school .
+docker build -t school ./dockerfiles/webapi/linux/
